8b3882c906214ad9034c6c33bc1350c98f1dc98c
yosymelnick <yosymelnick@gmail.com>
2026-03-26 14:57:36 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 
 const app = express();
 
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
